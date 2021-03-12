6b4686c6e2c046e0025a88439eea0306305d5ce4
andrewsu31098 <chronicles31098@gmail.com>
2021-03-11 21:04:05 -0800

    Learned how passport, passport-local, and passport-mongoose works. Passport sets very convenient frameworks for logging in, logging out, authenification of users, and even has a framework for failure messages. Passport-mongoose is what allows passport to authenticate and check users against the mongoose database so easily. Passport-local is also an important piece in this process, enabling the entire authentification process. Request sessions used to keep track of interesting things, such as where the user attempted to go as he was logged out.  Keeping a logged in user's user._id in session data is a good way to check if he is still logged in as well as accessing any of his data if necessary.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const catchAsync = require("../utils/catchAsync");
-const ExpressError = require('../utils/ExpressError');
-
-const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
 
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -24,11 +23,12 @@ router.get('/', async (req, res) => {
     res.render("campgrounds/index", { campgrounds });
 })
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
+
     res.render("campgrounds/new");
 })
 
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
 
@@ -50,7 +50,7 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render("campgrounds/show", { campground });
 }));
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
 
     if (!campground) {
@@ -61,7 +61,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
     res.render("campgrounds/edit", { campground });
 }))
 
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
 
@@ -70,7 +70,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
 
